12b3e3aa035c37fb61c05eb9bf11c3a68ff68641
Vlad Cebotari <vladcebotari789@gmail.com>
2025-11-20 05:06:21 +0200

    Implemented group chat feature(all connected client talk to each other)

--- client.cpp
+++ client.cpp
@@ -4,6 +4,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/select.h>
+#include <string.h>
+#include <arpa/inet.h>
+
 
 #define MAX_NUMBER_OF_REQUESTS 10
 #define PORT 8008
@@ -14,32 +18,49 @@ int setup_client();
 int main(){
   int client_socket_fd;
   client_socket_fd = setup_client();
-  
+  fd_set readfds;
 
  
   while(true){
-    //recv(client_socket_fd);
-    // send 
+      FD_ZERO(&readfds);
+      FD_SET(0,&readfds);
+      FD_SET(client_socket_fd,&readfds);
+      int maxfd = client_socket_fd;
+      int bytes;
+    select(maxfd+1,&readfds,NULL,NULL,NULL);  
+    
+    if(FD_ISSET(client_socket_fd,&readfds)){
+        char buffer[256];
+        bytes = recv(client_socket_fd,buffer,sizeof(buffer)-1,0);
+        buffer[bytes] = '\0';
+        printf("%s",buffer);
+
+      }
+    if(FD_ISSET(0,&readfds)){
+        char buffer[256];
+        fgets(buffer,sizeof(buffer),stdin);
+        send(client_socket_fd,buffer,strlen(buffer),0);
+      }
+
   }
 
   return 0;
 }
 
 int setup_client(){
-  struct sockaddr_in client_adress;
-  client_adress.sin_family = AF_INET;
-  client_adress.sin_addr.s_addr = INADDR_ANY;
-  client_adress.sin_port = htons(8008);
+  struct sockaddr_in server_adress;
+  server_adress.sin_family = AF_INET;
+  server_adress.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server_adress.sin_port = htons(8008);
 
   int client_socket_fd = socket(AF_INET,SOCK_STREAM,0);
  
-  int connection_status = connect(client_socket_fd,(struct sockaddr *)&client_adress,sizeof(client_adress));
+  int connection_status = connect(client_socket_fd,(struct sockaddr *)&server_adress,sizeof(server_adress));
   
-  if (connection_status = -1){
-    printf("Client failed to connect\n");
-
+  if (connection_status == -1){
+    perror("connect issue");
+    exit(1);
   }
 
   return client_socket_fd;
 }
-//
--- server.cpp
+++ server.cpp
@@ -4,27 +4,87 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/select.h>
+#include <string.h>
+
 
 #define MAX_NUMBER_OF_REQUESTS 10
 #define PORT 8008
 
 int setup_server();
-void *handle_connection(int client_fd);
+
 
 int main(int argc,char *argv[]){
   int clients_fd[MAX_NUMBER_OF_REQUESTS];
-  int number_of_clients = 0;
   int server_socket_fd;
+  int new_client;
+  fd_set readfds;
+  
+  struct sockaddr_in client_addr;
+  socklen_t addrlen = sizeof(client_addr);
 
   server_socket_fd = setup_server();
 
+  for(int i=0;i<MAX_NUMBER_OF_REQUESTS;i++){
+      clients_fd[i] = -1;
+  }
+      
   while(true){
-    int client_fd = accept(server_socket_fd,NULL,NULL);
-    // handle_connection(client_fd);
+    FD_ZERO(&readfds);
+    FD_SET(server_socket_fd,&readfds);
+    int maxfd = server_socket_fd;
+
+    //daca fd !=-1(adica clientul exista)il adaugam in FD_SET si gasim maxfd din toate fds 
+      for(int i=0;i<MAX_NUMBER_OF_REQUESTS;i++){
+        if(clients_fd[i] != -1){
+          FD_SET(clients_fd[i],&readfds);
+            if(clients_fd[i] >maxfd){
+              maxfd = clients_fd[i];
+            }
+        }
+      }
+
+  
+    select(maxfd+1,&readfds,NULL,NULL,NULL);
     
-    
-    clients_fd[number_of_clients++] = client_fd;
-    //send receive how and so on
+    if(FD_ISSET(server_socket_fd,&readfds)){
+      new_client = accept(server_socket_fd,(struct sockaddr*)&client_addr,&addrlen);
+      printf("New client connected (fd: %d)\n", new_client);
+
+        for(int i=0;i<MAX_NUMBER_OF_REQUESTS;i++){
+            if(clients_fd[i] == -1){
+              clients_fd[i] = new_client;
+              break;
+            }
+          }
+      }
+
+
+    for(int i = 0;i < MAX_NUMBER_OF_REQUESTS;i++){
+      if(clients_fd[i] == -1) continue;
+
+      if(FD_ISSET(clients_fd[i],&readfds)){
+      char buffer[256];
+      int bytes = recv(clients_fd[i],buffer,sizeof(buffer)-1,0);
+
+        if(bytes <=0){
+          printf("Client %d disconnected\n", clients_fd[i]); 
+          close(clients_fd[i]);
+          clients_fd[i] = -1;
+          continue;
+        }
+        buffer[bytes] ='\0';
+        printf("Client %d sent: %s", clients_fd[i], buffer);
+      
+        for(int j=0;j<MAX_NUMBER_OF_REQUESTS;j++){
+          int outfd = clients_fd[j];
+          if(outfd !=-1 && outfd!=clients_fd[i]){
+            send(outfd,buffer,bytes,0);
+          }
+        }
+      
+      } 
+    }
   }
 
   close(server_socket_fd);
@@ -44,12 +104,8 @@ int setup_server(){
   bind(server_socket_fd,(struct sockaddr *)&server_adress,sizeof(server_adress));
   listen(server_socket_fd,MAX_NUMBER_OF_REQUESTS);
 
+  printf("Server is running on port %d\n",PORT);
   return server_socket_fd;
 }
 
 
-//void *handle_connection(int client_fd){
-
-
-//}
-//
